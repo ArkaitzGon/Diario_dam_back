e87e578c8ec3275251063455f67765b7e144639e
Sara D'Alessio <sdalessio@birt.eus>
2025-01-17 22:30:56 +0100

    arreglado error valoraciones

--- src/main/java/dam/backend/dto/ValoracionDTO.java
+++ src/main/java/dam/backend/dto/ValoracionDTO.java
@@ -2,9 +2,11 @@ package dam.backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ValoracionDTO {
 
 	private boolean votado;
